e5515c3a920e796102502d0cb0bc77a8fbfe5f03
slaily <slavov.iliyan96@gmail.com>
2021-02-02 18:54:31 +0200

    Add a test case for functionality that generates Color Frame

--- tests/test_frames.py
+++ tests/test_frames.py
@@ -7,3 +7,10 @@ def test_black_frame():
     frame = generate_black_frame(32, 32)
 
     assert not np.any(frame)
+
+
+def test_generate_color_frame():
+    frame = generate_color_frame(32, 32)
+    _, _, channels = frame.shape
+
+    assert channels == 3
