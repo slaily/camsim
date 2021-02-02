2e6f9f884203439f9b45a63a64271ddd7088517f
slaily <slavov.iliyan96@gmail.com>
2021-02-02 18:55:57 +0200

    Add functionality that generates Color Frame

--- src/frames.py
+++ src/frames.py
@@ -3,3 +3,12 @@ import numpy as np
 
 def generate_black_frame(height, width):
     return np.zeros((height, width, 3), np.uint8)
+
+
+def generate_color_frame(height, width):
+    return np.random.randint(
+        0,
+        high=256,
+        size=((height, width, 3)),
+        dtype=np.uint8
+    )
