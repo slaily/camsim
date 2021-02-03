7124d0a0b2ece438abe6139c7045fca2045ac98f
slaily <slavov.iliyan96@gmail.com>
2021-02-03 17:19:08 +0200

    Add test cases regarding FPS functionalities

--- tests/test_fps.py
+++ tests/test_fps.py
@@ -16,3 +16,23 @@ class TestFPS:
         self.fps.start()
 
         assert isinstance(self.fps.start_time, datetime)
+
+    def test_stop(self):
+        self.fps.stop()
+
+        assert isinstance(self.fps.end_time, datetime)
+
+    def test_update(self):
+        self.fps.update()
+
+        assert self.fps.total_frames == 1
+
+    def test_elapsed(self):
+        elapsed = self.fps.elapsed()
+
+        assert elapsed > 0
+
+    def test_fps(self):
+        fps = self.fps.fps()
+
+        assert isinstance(fps, float)
