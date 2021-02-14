ffdd36b000c16269b7c2352240da1e035278d583
slaily <slavov.iliyan96@gmail.com>
2021-02-14 09:56:03 +0200

    Refactor Camera module

--- src/camera.py
+++ src/camera.py
@@ -5,10 +5,10 @@ from .frames import generate_color_frame
 
 class Camera:
     def __init__(self):
-        self.frame_grabbed, self.frame = self.generate_frame()
+        self.frame_grabbed, self.frame = self.read()
         self.stopped = False
 
-    def generate_frame(self):
+    def read(self):
         sleep(0.1)
 
         try:
@@ -20,13 +20,13 @@ class Camera:
 
             return False, None
 
-    def read(self):
+    def stream(self):
         while not self.stopped:
             if not self.frame_grabbed:
                 self.stop()
                 break
 
-            self.frame_grabbed, self.frame = self.generate_frame()
+            self.frame_grabbed, self.frame = self.read()
 
         return None
 
